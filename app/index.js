8c6e43373c1827f1726112897c21567d4c1f68b2
wilsung <wou123@yahoo.com>
2020-01-17 19:19:39 -0800

    created layout

--- app/index.js
+++ app/index.js
@@ -4,14 +4,33 @@ import './index.css'
 import Navigation from './components/Navigation'
 import 'tachyons'
 import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 500
+            }
+        }
+    }
+}
 
 class App extends React.Component{
     render(){
         return(
             <div>
+                <Particles 
+                    className='particles'
+                    params={particleOptions} 
+                />
                 <Navigation />
                 <Logo />
+                <Rank />
                 <ImageLinkForm />
                 {/* <FaceRecognition /> */}
             </div>
