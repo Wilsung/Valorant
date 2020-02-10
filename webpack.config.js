002a2f564db58ce5fc096529b2282157e9862bf7
wilsung <wou123@yahoo.com>
2020-02-09 19:00:58 -0800

    fixed deployment issues

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'app/index.html'
+            template: './app/index.html'
         }),
         // new CopyPlugin([
         //     { from: '_redirects'}
